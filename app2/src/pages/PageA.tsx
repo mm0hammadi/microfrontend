8d3951b870d9765e6f364d1898e689438ed110a6
mm0hammadi <mm34549@gmail.com>
2023-10-10 01:07:27 +0330

    Update PageA.tsx

--- app2/src/pages/PageA.tsx
+++ app2/src/pages/PageA.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export function PageA() {
+  
   return (
     <React.Fragment>
      <div style={{ height: "100vh", marginTop: "300px" }}>Page 1 from App2</div>
